8a7420e7f014b3ec04042e1a61b7ba64ecdc0b1c
shayanmc <shayanmc.dev@gmail.com>
2026-02-14 10:31:19 +0330

    add json books

---
+++ src/components/book.jsx
@@ -0,0 +1,25 @@
+import img1 from "./images/image1.jpg";
+import img2 from "./images/image2.jpg";
+import img3 from "./images/image3.jpg";
+
+export const book = [
+  {
+    author: "Rachel ReidRachel Reid",
+    title: "Heated Rivalry",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Hugh Laurie",
+    title: "Harry Potter",
+    img: img2,
+    id: 2,
+  },
+
+  {
+    author: "Freida McFadden",
+    title: "Dear Debbie",
+    img: img3,
+    id: 3,
+  },
+];
